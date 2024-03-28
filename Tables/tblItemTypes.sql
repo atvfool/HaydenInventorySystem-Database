a8bc79db26f3f2272c50cb1587f34dc0f27aaa8b
Andrew Hayden <atvfool@gmail.com>
2024-03-27 22:40:22 -0400

    Schema Updates

--- Tables/tblItemTypes.sql
+++ Tables/tblItemTypes.sql
@@ -1,12 +1,13 @@
 CREATE TABLE `tblItemTypes` (
-	`intID` INT(10) UNSIGNED NOT NULL,
+	`intID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`strName` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
 	`strDescription` VARCHAR(2000) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
-	`intParentTypeID` INT(11) UNSIGNED NOT NULL DEFAULT '0',
+	`intParentTypeID` INT(11) UNSIGNED NULL DEFAULT NULL,
 	PRIMARY KEY (`intID`) USING BTREE,
 	INDEX `FK_tblItemTypes_intParentID_intID` (`intParentTypeID`) USING BTREE,
 	CONSTRAINT `FK_tblItemTypes_intParentID_intID` FOREIGN KEY (`intParentTypeID`) REFERENCES `tblItemTypes` (`intID`) ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
+AUTO_INCREMENT=2
 ;
--- Tables/tblLocations.sql
+++ Tables/tblLocations.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `tblLocations` (
-	`intID` INT(10) UNSIGNED NOT NULL,
+	`intID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`strName` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
 	`strDescription` VARCHAR(2000) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
-	`intParentLocationID` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`intParentLocationID` INT(10) UNSIGNED NULL DEFAULT NULL,
 	PRIMARY KEY (`intID`) USING BTREE,
-	INDEX `FK_tblLocations_intParentLocationID_intID` (`intParentLocationID`) USING BTREE,
-	CONSTRAINT `FK_tblLocations_intParentLocationID_intID` FOREIGN KEY (`intParentLocationID`) REFERENCES `tblLocations` (`intID`) ON UPDATE NO ACTION ON DELETE NO ACTION
+	INDEX `FK_tblLocations_intParentLocationID_intID` (`intParentLocationID`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
+AUTO_INCREMENT=7
 ;
