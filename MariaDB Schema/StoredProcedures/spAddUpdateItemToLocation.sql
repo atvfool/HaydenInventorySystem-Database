0c3ff26f2df13771bec0817cfffa9fb66c104eb7
Andrew Hayden <atvfool@gmail.com>
2024-04-20 22:29:31 -0400

    Updates spAddUpdateItemToLocation

--- MariaDB Schema/StoredProcedures/spAddUpdateItemToLocation.sql
+++ MariaDB Schema/StoredProcedures/spAddUpdateItemToLocation.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`root`@`%` PROCEDURE `spAddUpdateItemToLocation`(
-	IN `strUPC` VARCHAR(50),
-	IN `intLocationID` INT
+	IN `strUPC_p` VARCHAR(50),
+	IN `intLocationID_p` INT
 )
 LANGUAGE SQL
 NOT DETERMINISTIC
@@ -8,13 +8,13 @@ CONTAINS SQL
 SQL SECURITY DEFINER
 COMMENT ''
 BEGIN
-	SET @intID= (SELECT intID FROM tblItems WHERE strUPC = strUPC LIMIT 1);
+	SET @intID= (SELECT intID FROM tblItems WHERE strUPC = strUPC_p LIMIT 1);
 	-- SELECT @intID;
 	IF @intID IS NULL THEN
 		SELECT 'ITEM NOT FOUND';
 	ELSE
 		INSERT INTO tblItemLocations(intItemID, intLocationID, intQty)
-		VALUES(@intID, intLocationID, 1)
+		VALUES(@intID, intLocationID_p, 1)
 		ON DUPLICATE KEY
 		UPDATE intQty = intQty + 1;
 		
